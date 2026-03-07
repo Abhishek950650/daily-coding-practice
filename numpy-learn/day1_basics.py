d87f2e39c1d904cd681ac3d5b64e1b30c4c584d6
Abhishek Kumar <abhishek950650@gmail.com>
2026-03-07 17:48:48 +0530

    commit numpy

---
+++ numpy-learn/day1_basics.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+# arr = np.array([1,2,3,4,5])
+
+# print(arr)
+
+# arr = np.array([1,2,3,4])
+
+# print(arr * 2)
+# print(arr + 10)
+
+# calculate mean
+# print(np.mean(arr))
+
+# matrix = np.array([
+#     [1,2,3],
+#     [4,5,6]
+# ])
+
+# print(matrix)
+# print(matrix.shape)
+
+data = [5,10,15,20]
+
+nparr = np.array(data)
+
+print(nparr)
+print(nparr * 3)
+print(np.mean(nparr))
+
