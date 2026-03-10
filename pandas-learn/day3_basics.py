839ebacc0aa18d9d4bbdd203ffd256d8abe41753
Abhishek Kumar <abhishek950650@gmail.com>
2026-03-10 21:57:43 +0530

    updated day3 learning complete of pandas

--- pandas-learn/day3_basics.py
+++ pandas-learn/day3_basics.py
@@ -11,3 +11,13 @@ salaries = [
 [2,50000],
 [3,60000]
 ]
+
+df1 = pd.DataFrame(employees, columns=['EmpId', 'Name'])
+df2 = pd.DataFrame(salaries, columns=['EmpId', 'Salary'])
+
+mergedf = pd.merge(df1, df2, on='EmpId', how='outer')
+# print(mergedf)
+
+mergedf['Salary_Level'] = mergedf['Salary'].apply(lambda x: 'High' if x > 55000 else ('Low' if x < 45000 else 'Medium'))
+
+print(mergedf)
